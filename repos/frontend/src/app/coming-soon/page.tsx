184cda86ad49c09cde7f886c9413877c7553972d
Martin Petr <martinpetrnp@gmail.com>
2026-06-09 21:39:17 +0200

    Make coming soon page responsive

--- repos/frontend/src/app/coming-soon/page.tsx
+++ repos/frontend/src/app/coming-soon/page.tsx
@@ -1,7 +1,9 @@
 export default function Page() {
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-ctp-base">
-      <h1 className="text-8xl font-black text-ctp-text">Coming soon...</h1>
+      <h1 className="text-4xl font-black text-ctp-text md:text-6xl lg:text-8xl">
+        Coming soon...
+      </h1>
     </div>
   );
 }
